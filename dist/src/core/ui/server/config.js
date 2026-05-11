ed19847a8858a3647204bae4a3fa3b6d39480ff0
mohamed amine nour <medaminenour@gmail.com>
2026-05-11 22:50:15 +0200

    fix(dist): clean re build

--- dist/src/core/ui/server/config.js
+++
@@ -1,1 +0,0 @@
-export { CONFIG_DIR, CONFIG_FILE, readConfig, saveConfig, readTheme, readViewMode, readVerbose, readMinSeverity, readModel, readThinking, readAutoCollapseViewed, readDefaultBranch, } from "../../config.js";
